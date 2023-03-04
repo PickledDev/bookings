01720b6760c6acabc706802abfa12c44f03c7ba1
Michael Irwin <pickledmisfit@gmail.com>
2023-03-04 08:26:09 -0600

    added database models

--- internal/models/models.go
+++ internal/models/models.go
@@ -10,6 +10,7 @@ type Reservation struct {
 	Phone     string
 }
 
+// Users is the users model
 type Users struct {
 	ID          int
 	FirstName   string
@@ -19,4 +20,50 @@ type Users struct {
 	AccessLevel int
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
+}
+
+// Rooms is the the rooms model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is is restrictions model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestrictions is the room restriction model
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
